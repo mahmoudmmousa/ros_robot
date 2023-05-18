eea66ef2f77de082005ae27d403d8e24dfca144d
Mahmoud Mousa <34664574+mahmoudmmousa@users.noreply.github.com>
2023-05-18 20:10:58 +0300

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mahmoud/ghazy_ws/src'
+whitelisted_packages = 'odom_pub'.split(';') if 'odom_pub' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mahmoud/ghazy_ws/devel;/home/mahmoud/catkin_ws/devel;/home/mahmoud/hospital_ws/devel;/home/mahmoud/ROS_Word_Space/devel;/opt/ros/noetic'.split(';') if '/home/mahmoud/ghazy_ws/devel;/home/mahmoud/catkin_ws/devel;/home/mahmoud/hospital_ws/devel;/home/mahmoud/ROS_Word_Space/devel;/opt/ros/noetic' != '' else []
