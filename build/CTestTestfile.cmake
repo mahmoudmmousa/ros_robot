f7ccf7c325a05a46e2e280811d7c43d4a4267792
Mahmoud Mousa <34664574+mahmoudmmousa@users.noreply.github.com>
2023-05-18 20:08:16 +0300

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mahmoud/ghazy_ws/src
+# Build directory: /home/mahmoud/ghazy_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("odom_pub")
